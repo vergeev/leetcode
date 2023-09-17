9c73783913575ca8df39c03d6ca00ec02dfbb218
Pavel Vergeev <vernier_06_curios@icloud.com>
2023-09-17 20:15:31 +0400

    add requirements, pre-commit, solution_27

---
+++ solution_27.py
@@ -0,0 +1,6 @@
+"""https://leetcode.com/problems/remove-element/?envType=study-plan-v2&envId=top-interview-150"""
+from typing import List
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        pass
