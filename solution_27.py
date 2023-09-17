a948b2b906fb7fd63d51c377c897ca5cfbdf10f1
Pavel Vergeev <vernier_06_curios@icloud.com>
2023-09-17 21:10:59 +0400

    remove unfinished solution

--- solution_27.py
+++
@@ -1,6 +0,0 @@
-"""https://leetcode.com/problems/remove-element/?envType=study-plan-v2&envId=top-interview-150"""
-from typing import List
-
-class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        pass
